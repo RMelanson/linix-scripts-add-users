d6c497115e89f9832e646c156002364ff1a99e2b
Robin Melanson <remcogold@gmail.com>
2019-03-14 10:57:12 -0400

    Create setEnv.sh

---
+++ env/setEnv.sh
@@ -0,0 +1,6 @@
+# Add Users
+pkg=ADD_USERS
+appServicesDir=/etc/init.d
+appProcessDir=$appServicesDir/appProcesses
+daemon=appServices.d
+testApp=" java -jar $appProcessDir/springBootHelloWorldDemo-0.1.0.jar"

0e0c06c296bab3e6a9381bbc9a334bc6a392a691
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:28:26 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,5 +1,7 @@
 # Add Users
 pkg=ADD_USERS
+gitRepo="linux-scripts-add-users"
+installDir="/tmp/scripts/utils/$pkg"
 appServicesDir=/etc/init.d
 appProcessDir=$appServicesDir/appProcesses
 daemon=appServices.d

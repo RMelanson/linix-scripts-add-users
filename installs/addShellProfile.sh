3567e7db622f46acffddcc3de826724738fa01c9
Robin Melanson <remcogold@gmail.com>
2019-03-18 22:36:25 -0400

    Update addShellProfile.sh

--- installs/addShellProfile.sh
+++ installs/addShellProfile.sh
@@ -1,2 +1,14 @@
 shell=$1
 echo "Adding User Shell Profile $shell"
+
+      case "$shell" in
+        basch)
+              ./installs/addPassword.sh $value
+              ;;
+        profile)
+              ./installs/addSSH_PubKey.sh $value
+              ;;
+       *) echo Unknown skell profile = $key with value $value
+             exit 1
+             ;;
+       esac

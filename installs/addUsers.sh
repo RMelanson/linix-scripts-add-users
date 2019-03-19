09f888f1e8a61ec088335616bf5a22d58fa7d710
Robin Melanson <remcogold@gmail.com>
2019-03-18 22:10:24 -0400

    Update addUsers.sh

--- installs/addUsers.sh
+++ installs/addUsers.sh
@@ -13,7 +13,27 @@ function processUserFile {
    while read key value
    do
       echo processing User property key = $key with value $value
+
+      case "$key" in
+        password)
+              ./installs/password.sh $value
+              ;;
+        sshPubKey)
+              ./installs/sshPubKey.sh $value
+              ;;
+        sudo)
+              ./installs/sudo.sh $value
+              ;;
+        shell)
+              ./installs/addShell.sh $value
+              ;;
+       *) echo Unknown property key = $key with value $value
+             exit 1
+             ;;
+       esac
+       
    done < "$userFile"
+   exit 0
 }
 
 #PROCESS USERS

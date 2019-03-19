2d02e70e5d157da13db594521f3af728cb4cd294
Robin Melanson <remcogold@gmail.com>
2019-03-18 21:06:22 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,7 +1,6 @@
 # Add Users
 pkg=ADD_USERS
 gitRepo="linux-scripts-utils-add-users"
-installDir="/tmp/scripts/utils/$pkg"
-USERS=$installDir/users/*
-KEYS=$installDir/keyLocker
-SHELL=$installDir/shell
+USERS=./$pkg/users/*
+KEYS=./$pkg/keyLocker
+SHELL=./$pkg/shell

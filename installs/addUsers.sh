8bd50833ace679b8f06932fc23b903a3cfa9eb88
Robin Melanson <remcogold@gmail.com>
2019-03-20 01:34:27 -0400

    Update addUsers.sh

--- installs/addUsers.sh
+++ installs/addUsers.sh
@@ -4,39 +4,10 @@
 # SETUP WILDFLY CONFIGURATION ENVIRONMENT AND PARAMETERS
 . ./env/setEnv.sh
 
-function processUserFile {
-   userFile=$1
-   user=$(echo $userFile | rev | cut -d/ -f1 | rev)
-   ./installs/addUser.sh $user
-   while read key value
-   do
-      echo processing User $user property key = $key with value $value
-
-      case "$key" in
-        password)
-              ./installs/addPassword.sh $user $value
-              ;;
-        sshPubKey)
-              ./installs/addSSH_PubKey.sh $user $value
-              ;;
-        sudo)
-              ./installs/addSudoAccess.sh $user
-              ;;
-        shell)
-              ./installs/addShellProfile.sh $user $value
-              ;;
-       *) echo Unknown property key = $key with value $value
-             exit 1
-             ;;
-       esac
-
-   done < "$userFile"
-   exit 0
-}
-
 #PROCESS USERS
 for userFile in $USERS
 do
-  # Add Users defined by UserFile
-  processUserFile $userFile
+  user=$(echo $userFile | rev | cut -d/ -f1 | rev)
+  # Add User $user defined in UserFile $userFile
+  ./installs/addUser.sh $user
 done

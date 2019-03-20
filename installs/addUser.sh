ee3e4682c180d62ed6f98e51fad9ae63e30aafcf
Robin Melanson <remcogold@gmail.com>
2019-03-20 02:11:30 -0400

    Update addUser.sh

--- installs/addUser.sh
+++ installs/addUser.sh
@@ -1,45 +1,60 @@
-   user=$1
-   echo "Adding User $user"
-
 if [ "$#" -lt 1 ]
 then
-    echo "Illegal number of parameters $#"
+    echo "USER Not specified ~ Illegal number of parameters $#"
     return
-else
-   user=$1
 fi
 
-if [ "$#" -lt 2 ]
-then
-   shell=bash
-else
-   user=$2
+USER=$1
+
+if grep -q $USER "/etc/passwd"; then
+   echo User $USER exists: \*\*\* Not adding \*\*\*
+   return
 fi
 
-echo Adding user=$user with shell $shell for pkg $pkg
+echo "Adding User $USER"
+ 
+USER_FILE=$USERS/$USER
+
+echo PROCESSING USERFILE $USER_FILE
+
+while read key value
+   do
+      echo processing User $USER property key = $key with value $value
+
+      case "$key" in
+        password)
+              PASSWORD="-p $value"
+              ;;
+        home)
+              HOME_DIR="-d /home/$value"
+              ;;
+        sshPubKey)
+              SSH_KEY=$value
+              ;;
+        sudo)
+              ./installs/addSudoAccess.sh $USER
+              ;;
+        shell)
+              SHELL="-s /bin/$value"
+              ;;
+       *) echo Unknown property key = $key with value $value
+             exit 1
+             ;;
+       esac
+
+done < "$USER_FILE"
+
+echo Adding user=$USER with shell $shell for pkg $pkg
 
 #Check if $pkg admin user exists
-if grep -q $user "/etc/passwd"; then
-   echo User $user exists: \*\*\* Not adding \*\*\*
+if grep -q $USER "/etc/passwd"; then
+   echo User $USER exists: \*\*\* Not adding \*\*\*
 else
-   #Create $pkg admin user $user and $pkg group $group
+   #Create $pkg admin user $USER and $pkg group $group
    #Check if user directory exists and create it if it does not
-   if [ ! -d $home ]
-   then
-      echo $home does not exist \*\*\* CREATING \*\*\*
-      mkdir -p $home
-   fi
-   echo Adding $pkg admin user $user to group $group
-   if grep -q $group /etc/group
-   then
-        echo "group exists"
-   else
-        echo "Creating group $group"
-        groupadd $group
-   fi
 
-   useradd -M -s /bin/bash -g $group -d $home $user
-   chown $user:$group $home
+   useradd -M -s /bin/bash -g $group -d $home $USER
+   chown $USER:$group $home
    #Add $user ssh access
    cp -r ~ec2-user/.ssh $home
 fi

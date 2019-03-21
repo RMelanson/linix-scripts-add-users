4abd8d63231ba962100cf625a08bdaa787c78d16
Robin Melanson <remcogold@gmail.com>
2019-03-20 22:04:19 -0400

    Update addSSH_Security.sh

--- installs/addSSH_Security.sh
+++ installs/addSSH_Security.sh
@@ -1,3 +1,12 @@
 user=$1
 keyPvtFile=$2
-echo "Adding keyPvtFile $keyPvtFile"
+
+echo "Adding User $user keyPvtFile $keyPvtFile"
+echo EXECUTING COMMAND mkdir ~$USER/.ssh
+mkdir ~$USER/.ssh
+echo EXECUTING COMMAND mkdir
+cp ./keyLocker/$value ~$USER/.ssh/authorized_keys
+echo EXECUTING COMMAND mkdir
+chown -R $USER ~$USER/.ssh; chmod 700 ~$USER/.ssh
+echo EXECUTING COMMAND mkdir
+chmod 600 ~$USER/.ssh/authorized_keys

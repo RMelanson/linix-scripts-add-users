362b6a52e33fc72808bb9eea1949da5912431cea
Robin Melanson <remcogold@gmail.com>
2019-03-22 00:50:20 -0400

    Update addUserTest.sh

--- addUserTest.sh
+++ addUserTest.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Setup the required environment
 . ./env/setEnv.sh
-
+sshKey=./KeyLocker/developers
 #------------------- INITIAL INSTALL --------------------
 . ./installs/addUserTest.sh joe

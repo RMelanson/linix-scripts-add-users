fffde1e5fed2a98a3cc30eb56bbbeef75b17bc07
Robin Melanson <remcogold@gmail.com>
2019-03-22 01:37:38 -0400

    Update addDevUser.sh

--- addDevUser.sh
+++ addDevUser.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 # Setup the required environment
+
+function addUser {
+   echo "Adding Dev User $1"
+   . ./installs/addDevUser.sh $1 $sshDevKey
+}
+
+if [ $# -eq 0 ]
+  then
+       echo "No user arguments supplied"
+  else
+       user=$1
+       addUser
+fi
+
 . ./env/setEnv.sh
-user=$1
-#------------------- INITIAL INSTALL --------------------
-if []
-. ./installs/addDevUser.sh $user

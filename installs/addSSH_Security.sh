1855a12cc5d858ab6b4e3d7bb3623a958119d7d2
Robin Melanson <remcogold@gmail.com>
2019-03-24 17:04:49 -0400

    Update addSSH_Security.sh

--- installs/addSSH_Security.sh
+++ installs/addSSH_Security.sh
@@ -8,7 +8,9 @@ echo EXECUTING COMMAND mkdir $HOME_DIR/.ssh
 mkdir $HOME_DIR/.ssh
 echo EXECUTING COMMAND cp ./keyLocker/$value $HOME_DIR/.ssh/authorized_keys
 cp ./keyLocker/$keyPvtFile $HOME_DIR/.ssh/authorized_keys
-echo EXECUTING COMMAND chown -R $USER:$USER $HOME_DIR/.ssh; chmod 700 ~$HOME_DIR/.ssh
-chown -R $USER:$USER $HOME_DIR/.ssh; chmod 700 $HOME_DIR/.ssh
+echo EXECUTING COMMAND chown -R $USER:$USER $HOME_DIR/.ssh
+chown -R $USER:$USER $HOME_DIR/.ssh; 
+echo chmod 700 $HOME_DIR/.ssh
+chmod 700 $HOME_DIR/.ssh
 echo EXECUTING COMMAND chmod 600 $HOME_DIR/.ssh/authorized_keys
-chmod 600 ~$HOME_DIR/.ssh/authorized_keys
+chmod 600 $HOME_DIR/.ssh/authorized_keys

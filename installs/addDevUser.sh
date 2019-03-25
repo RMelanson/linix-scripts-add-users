31f02d0f43484c0519d8e945632161165f187b20
Robin Melanson <remcogold@gmail.com>
2019-03-24 22:54:32 -0400

    Update addDevUser.sh

--- installs/addDevUser.sh
+++ installs/addDevUser.sh
@@ -18,8 +18,8 @@ if grep -q $USER "/etc/passwd"; then
    exit
 fi
 
-echo "====== ADDING NEW USER $USER ======"
 echo ########################################################################################
+echo "====== ADDING NEW USER $USER ======"
  
 echo PROCESSING USERFILE $USER_FILE
 
@@ -27,23 +27,23 @@ PASSWORD="-p $(openssl passwd -1 "ineedtolearn")"
 
 SHELL="-s /bin/bash"
 
-
 echo USER = $USER
 echo PASSWORD = $PASSWORD
 echo SHELL = $SHELL
 ADD_NEW_USER="useradd $USER $PASSWORD $SHELL"
-echo ADD_NEW_USER = $ADD_NEW_USER
-
 
 echo EXECUTING $ADD_NEW_USER
 $ADD_NEW_USER
 
-echo ########################################################################################
-echo "====== ADDING NEW USER $USER SSH SECURITY ======"
+echo ----------------------------------------------------------------------------------------
+echo "====== Extracting $USER Home Directiry ======"
 
 echo EXECUTING "HOME_DIR=$(eval echo ~$USER)"
 HOME_DIR=$(eval echo ~$USER)
 echo HOME_DIR = $HOME_DIR
+
+echo ----------------------------------------------------------------------------------------
+echo "====== ADDING NEW USER $USER SSH SECURITY ======"
 
 ADD_SSH_SECURITY=". ./installs/addSSH_Security.sh $USER $sshDevKey"
 
@@ -51,10 +51,11 @@ echo EXECUTING $ADD_SSH_SECURITY
 $ADD_SSH_SECURITY
 
 
-echo ########################################################################################
+echo ----------------------------------------------------------------------------------------
 echo "====== ADDING NEW USER $USER SUDO ACCESS ======"
 
 ADD_SUDO_ACCESS=". ./installs/addSudoAccess.sh"
 
 echo EXECUTING $ADD_SUDO_ACCESS
 $ADD_SUDO_ACCESS
+echo ########################################################################################

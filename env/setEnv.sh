72dfa1c672cd29ace4fd1ad12ad9278bcdcdb097
Robin Melanson <remcogold@gmail.com>
2019-04-22 13:03:49 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,8 +1,13 @@
 # Add Users
-pkg=ADD_USERS
 bootstrap="addUsersBootstraps.sh"
-
 gitRepo="linux-scripts-utils-add-users"
+
+#SET UP INSTALLATION DIRECTORY`
+pkg=ADD_USERS
+scriptType="utils"
+parentDir="/tmp/scripts/$scriptType/"
+installDir="$parentDir/$pkg"
+
 sshDevKey=./keyLocker/developers
 USERS=./users/*
 KEYS=./keyLocker
